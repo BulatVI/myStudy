d94fe448d952c789ff749d6bce8681b9069145ee
Bulat Val <bulat1987@gmail.com>
2021-12-18 16:22:01 +0500

    Create first java file and init commit

---
+++ src/com/BulatVI/stepik/functionalProgramming/HelloB.java
@@ -0,0 +1,7 @@
+package com.BulatVI.stepik.functionalProgramming;
+
+public class HelloB {
+    public static void main(String[] args) {
+        System.out.println("Hello from Bulat!!!");
+    }
+}
