1f28cd83c2449671d605ac2889ef885497a1e878
Bulat Val <bulat1987@gmail.com>
2021-12-18 16:32:01 +0500

    Second commit

--- src/com/BulatVI/stepik/functionalProgramming/HelloB.java
+++ src/com/BulatVI/stepik/functionalProgramming/HelloB.java
@@ -3,5 +3,6 @@ package com.BulatVI.stepik.functionalProgramming;
 public class HelloB {
     public static void main(String[] args) {
         System.out.println("Hello from Bulat!!!");
+        System.out.println("Hello world");
     }
 }
