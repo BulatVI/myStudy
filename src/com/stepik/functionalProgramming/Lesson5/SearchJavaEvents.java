d51d116cb6f484c1cac0ae86990fc544edfb8ad4
Bulat Val <bulat1987@gmail.com>
2021-12-28 21:18:38 +0500

    24.12.2021  -  solve Task 5.2

---
+++ src/com/stepik/functionalProgramming/Lesson5/SearchJavaEvents.java
@@ -0,0 +1,48 @@
+package com.stepik.functionalProgramming.Lesson5;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Here you need to implement the printSortedJavaEvents method that takes a list of event titles and must print only
+ * the names of Java-related events in the alphabetical order.
+ * An event is considered a Java-related if it contains the substring "Java" in the title.
+ * It is important that this substring can be written in any case: "java", "Java", "JAVA", etc, and they all must count.
+ * <pre>{@code
+ * Sample Input:
+ * JavaZone
+ * KotlinConf
+ * java night party | Amsterdam
+ * Day of Java
+ * PyCon US
+ *
+ * Sample Output:
+ * Day of Java
+ * JavaZone
+ * java night party | Amsterdam
+ * }</pre>
+ */
+class SearchJavaEvents {
+
+    public static void printSortedJavaEvents(List<String> events) {
+        // write your code here
+        events.stream()
+                .filter(string -> string.toLowerCase().contains("java"))
+                .sorted()
+                .forEach(System.out::println);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<String> events = List.of("JavaZone", "KotlinConf", "java night party | Amsterdam", "Day of Java", "PyCon US");
+                /*List<String> events = Stream
+                .iterate(1, i -> scanner.hasNextLine(), i -> i + 1)
+                .map(i -> scanner.nextLine())
+                .collect(Collectors.toList());*/
+
+        printSortedJavaEvents(events);
+    }
+}
