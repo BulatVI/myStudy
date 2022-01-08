84416d7ad61518268cd602c6614be102c4de945a
Bulat Val <bulat1987@gmail.com>
2022-01-08 21:48:42 +0500

    08.01.2022 -

--- src/com/stepik/functionalProgramming/Lesson7/Tasks1DesignPatterns/ChainOfResponsibilityDemo.java
+++ src/com/stepik/functionalProgramming/Lesson7/Tasks1DesignPatterns/ChainOfResponsibilityDemo.java
@@ -30,31 +30,45 @@ class ChainOfResponsibilityDemo {
     /**
      * Accepts a request and returns a new request with data wrapped in the tag <request>...</request>
      */
-    static RequestHandler wrapInRequestTag = req ->
-            new Request(String.format("<request>%s</request>", req.getData()));
+    static RequestHandler wrapInRequestTag = new RequestHandler() {
+        @Override
+        public Request handle(Request req) {
+            return new Request(String.format("<request>%s</request>", req.getData()));
+        }
+    };
 
     /**
      * It should represents a chain of responsibility combined from another handlers.
      * The format: commonRequestHandler = handler1.setSuccessor(handler2.setSuccessor(...))
      * The combining method setSuccessor may has another name
      */
-    static RequestHandler commonRequestHandler = // !!! write a combination of existing handlers here
+    static final RequestHandler commonRequestHandler = // !!! write a combination of existing handlers here
+            wrapInTransactionTag.andThen(createDigest).andThen(wrapInRequestTag);
+            /*request -> {
+                Request r1 = wrapInTransactionTag.handle(request);
+                Request r2 = createDigest.handle(r1);
+                return wrapInRequestTag.handle(r2);
+            };*/
 
     /**
      * It represents a handler and has two methods: one for handling requests and other for combining handlers
      */
     @FunctionalInterface
     interface RequestHandler {
+
         // !!! write a method handle that accept request and returns new request here
         // it allows to use lambda expressions for creating handlers below
+        public Request handle(Request request);
 
         // !!! write a default method for combining this and other handler single one
         // the order of execution may be any but you need to consider it when composing handlers
         // the method may has any name
-
-        Request handle(Request Request);
-
+        default RequestHandler andThen(RequestHandler other) {
+            //return request -> other.handle(this.handle(request));
+            return request-> other.handle(handle(request));
+        }
     }
+
     /**
      * Immutable class for representing requests.
      * If you need to change the request data then create new request.
